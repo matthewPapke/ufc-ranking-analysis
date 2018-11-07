c14b45d8c31c90bdbeefced913333ae7ac9b0a9b
matthewPapke <mpapke@jobcase.com>
2018-11-06 20:52:58 -0500

    initial plotting of win-rate

--- python/parse_fighter_data.py
+++ python/parse_fighter_data.py
@@ -7,6 +7,36 @@ contentStr = r.content
 
 fighters = json.loads(contentStr)
 
+def abbreviateWeightClass( weight_class ):
+	if weight_class == "Flyweight":
+		return "FLW"
+	elif weight_class == "Bantamweight":
+		return "BW"
+	elif weight_class == "Featherweight":
+		return "FW"
+	elif weight_class == "Lightweight":
+		return "LW"
+	elif weight_class == "Welterweight":
+		return "WW"
+	elif weight_class == "Middleweight":
+		return "MW"
+	elif weight_class == "Light_Heavyweight":
+		return "LHW"
+	elif weight_class == "Heavyweight":
+		return "HW"
+	elif weight_class == "Women_Strawweight":
+		return "WSW"
+	elif weight_class == "Women_Flyweight":
+		return "WFLW"
+	elif weight_class == "Women_Bantamweight":
+		return "WBW"
+	elif weight_class == "Women_Featherweight":
+		return "WFW"
+	else:
+		return "Weightless"
+	
+
+
 # get ranked fighters in each division
 rankedFighterInfo = []
 
@@ -17,11 +47,11 @@ for fighter in fighters:
 		fighterInfo['wins']   = fighter['wins']
 		fighterInfo['losses'] = fighter['losses']
 		fighterInfo['draws']  = fighter['draws']
-		fighterInfo['weight_class'] = fighter['weight_class']
+		fighterInfo['weight_class'] = abbreviateWeightClass(fighter['weight_class'])
 		fighterInfo['rank'] = fighter['rank']
 
 		rankedFighterInfo.append(fighterInfo)
 
 # write to file for plotting in R
 with open('../text/ranked_fighters.txt', 'w') as fout:
-    json.dump(your_list_of_dict, fout)
+    json.dump(rankedFighterInfo, fout)
---
+++ r/plot_win_rate.R
@@ -0,0 +1,20 @@
+library(jsonlite)  
+
+# deserialize ranked fighter json into data frame
+fighters = fromJSON(txt = "./text/ranked_fighters.txt")
+
+fighters$win_rate = fighters$wins/(fighters$wins + fighters$draws + fighters$losses)
+
+
+
+
+
+pd <- position_dodge(width = 0.5)
+g = ggplot(fighters, aes(x = weight_class, y = win_rate, colour = weight_class)) + geom_point(position = position_jitter(width = 0.2))
+g + theme(legend.position="none") 
+
+  # geom_errorbar(aes(ymax = high, ymin = lo), width = 0.15, position = pd)  +
+  # labs(x = 'cohort', y = 'CTR', title = 'win rate by weight class') 
+  # geom_jitter(width = 0.3)
+  
+gg_sl_main
