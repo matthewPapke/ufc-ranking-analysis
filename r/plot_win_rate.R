3e84e67abb9bc3ffbe006cefe1e9e531157eb842
matthewPapke <mpapke@jobcase.com>
2018-11-25 17:19:47 -0500

    add win-rate plot

--- r/plot_win_rate.R
+++ r/plot_win_rate.R
@@ -3,6 +3,11 @@ library(jsonlite)
 # deserialize ranked fighter json into data frame
 fighters = fromJSON(txt = "./text/ranked_fighters.txt")
 
+
+abbreviate_weight_class = function(weight_class) {
+  
+}
+
 fighters$win_rate = fighters$wins/(fighters$wins + fighters$draws + fighters$losses)
 
 
@@ -11,7 +16,7 @@ fighters$win_rate = fighters$wins/(fighters$wins + fighters$draws + fighters$los
 
 pd <- position_dodge(width = 0.5)
 g = ggplot(fighters, aes(x = weight_class, y = win_rate, colour = weight_class)) + geom_point(position = position_jitter(width = 0.2))
-g + theme(legend.position="none") 
+g + theme(legend.position="none")  + labs(x = "Weight class", y = "Win-rate", title = "UFC top 15 win-rate by weight class")
 
   # geom_errorbar(aes(ymax = high, ymin = lo), width = 0.15, position = pd)  +
   # labs(x = 'cohort', y = 'CTR', title = 'win rate by weight class') 
